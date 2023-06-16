0fe1b62f283e36ba8c4b832b40548517152911ad
ian <ian.maina@student.moringaschool.com>
2023-06-16 22:28:49 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const divide = function(){
+    let quotient = 2000/100
+    return quotient
+}
+console.log(divide())
+
+const square = num1 => num1*num1
+console.log(square(3000))
+
+const add = (x, y) => x + y
+console.log(add(450,500))
